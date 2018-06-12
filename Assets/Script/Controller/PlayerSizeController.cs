2aa4670015518713683a7091b8c0803ce5f127aa
MrXGJ <1849442942@qq.com>
2018-06-12 16:17:16 +0800

    增加玩家大小技能及速度技能组件

---
+++ Assets/Script/Controller/PlayerSizeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSizeController : Photon.PunBehaviour {
+
+    public float cooldown;//定义技能冷却时间
+    public Vector3 addSize;//增加的大小
+    public float keepTime;//技能效果持续时间
+
+    private float curCooldown;
+
+	// Use this for initialization
+	void Start () {
+        curCooldown = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //技能触发
+        if (Input.GetKeyDown("space") && curCooldown <= 0)
+        {
+            curCooldown = cooldown;
+            transform.localScale += addSize;
+            StartCoroutine("WaitForEndSkill");
+        }
+
+        if(curCooldown >= 0)
+        {
+            curCooldown -= Time.deltaTime;
+        }
+
+	}
+
+    IEnumerator WaitForEndSkill()
+    {
+        yield return new WaitForSeconds(keepTime);
+        transform.localScale -= addSize;
+    }
+
+}
