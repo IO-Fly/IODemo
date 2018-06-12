79334ec2e4c9e1dc1b00b1bb09790fdca4c02ec8
姚健钊 <1720231298@qq.com>
2018-06-12 17:43:45 +0800

    修改了PlayerController组件的逻辑
    修改了由玩家输入WSAD引起的角色方向改变的逻辑，角色转向时有平滑过渡，可以代替动画

--- Assets/Script/Controller/PlayerController.cs
+++ Assets/Script/Controller/PlayerController.cs
@@ -13,8 +13,8 @@ public class PlayerController : MonoBehaviour {
     private Vector3 right;
     private float speed;
 
-    private enum MOVE_DIRECTION { FRONT,FRONT_LEFT,FRONT_RIGHT, LEFT, RIGHT };
-    private MOVE_DIRECTION moveDirection;
+    private float currentLookAtSlerp;
+    private float targetLookAtSlerp;
 
     private CharacterController character;
 
@@ -24,8 +24,9 @@ public class PlayerController : MonoBehaviour {
         towards = new Vector3(0.0f, 0.0f, 1.0f);
         up = new Vector3(0.0f, 1.0f, 0.0f);
         right = new Vector3(1.0f, 0.0f, 0.0f);
-        moveDirection = MOVE_DIRECTION.FRONT;
         character = gameObject.GetComponent<CharacterController>();
+        currentLookAtSlerp = 0.5f;
+        targetLookAtSlerp = 0.5f;
 	}
 	
 	// Update is called once per frame
@@ -63,53 +64,48 @@ public class PlayerController : MonoBehaviour {
             up.Normalize();
         }
 
+        //处理玩家的WSAD输入（改变角色的方向）
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
+        //由WSAD决定的球面插值的参数（角色移动方向）
         Vector3 lookAt = new Vector3();
         if (moveHorizontal > 0.0f)
             if (moveVertical > 0.0f)
-                moveDirection = MOVE_DIRECTION.FRONT_RIGHT;
+                targetLookAtSlerp = 0.75f;
             else
-                moveDirection = MOVE_DIRECTION.RIGHT;
+                targetLookAtSlerp = 1.0f;
         else if (moveHorizontal < 0.0f)
             if (moveVertical > 0.0f)
-                moveDirection = MOVE_DIRECTION.FRONT_LEFT;
+                targetLookAtSlerp = 0.25f;
             else
-                moveDirection = MOVE_DIRECTION.LEFT;
+                targetLookAtSlerp = 0.0f;
         else if (moveVertical >= 0.0f)
-            moveDirection = MOVE_DIRECTION.FRONT;
-        switch (moveDirection)
-        {
-            case MOVE_DIRECTION.FRONT:
-                lookAt = towards;
-                break;
-            case MOVE_DIRECTION.FRONT_LEFT:
-                lookAt = Vector3.Slerp(towards, -right, 0.5f);
-                break;
-            case MOVE_DIRECTION.LEFT:
-                lookAt = -right;
-                break;
-            case MOVE_DIRECTION.FRONT_RIGHT:
-                lookAt = Vector3.Slerp(towards, right, 0.5f);
-                break;
-            case MOVE_DIRECTION.RIGHT:
-                lookAt = right;
-                break;
-        }
+            targetLookAtSlerp = 0.5f;
+
+        //平滑过渡（角色的转向）
+        if (targetLookAtSlerp - currentLookAtSlerp < 1e-6)
+            currentLookAtSlerp = targetLookAtSlerp;
+        else
+            currentLookAtSlerp += (targetLookAtSlerp - currentLookAtSlerp) * 0.4f;
+
+        //执行转向操作
+        lookAt = Vector3.SlerpUnclamped(-right, towards, currentLookAtSlerp*2);
         transform.LookAt(gameObject.transform.position + lookAt);
 
+        //限制不能往后移动
         if (moveVertical < 0.0f)
             moveVertical = 0.0f;
+
+        //执行移动操作
         Vector3 move = towards * moveVertical + right * moveHorizontal;
         speed = gameObject.GetComponent<Player>().GetSpeed();
         character.Move(move * speed);
-        //gameObject.transform.position += (move.normalized * speed);
-
     }
 
     public Vector3 GetTowards()
     {
         return towards;
     }
+
 }
