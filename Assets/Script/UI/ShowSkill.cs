7eaa0fc82e44293e1105b48acfd7a31e3e3cd0d4
zdsky <1583244339@qq.com>
2018-06-14 14:04:00 +0800

    增加技能冷却时间显示

---
+++ Assets/Script/UI/ShowSkill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowSkill : MonoBehaviour {
+
+    private GameObject player;
+    private Text coolTimeText;
+
+    void Start ()
+    {
+        coolTimeText = this.transform.Find("SkillImage/SkillTimeText").gameObject.GetComponent<Text>();
+
+        //Debug.Log(player.name);
+    }
+	
+	void Update ()
+    {
+        
+        coolTimeText.text = Mathf.Ceil(player.GetComponent<PlayerSizeController>().GetCurCoolTime()).ToString();
+	}
+
+    public void setPlayer(GameObject player)
+    {
+        this.player = player;
+    }
+}
