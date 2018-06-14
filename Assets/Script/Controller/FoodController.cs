7a40ddc839446c0cafc841132aca9b2c93275970
Onion <244081385@qq.com>
2018-06-14 14:05:32 +0800

    改变食物生成逻辑

--- Assets/Script/Controller/FoodController.cs
+++ Assets/Script/Controller/FoodController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class FoodController : Photon.PunBehaviour {
 	public GameObject foodPrefab;
+	private Vector3 translation;
 	public float rotationSpeed=2f;
+	public float translationSpeed=0.05f;
 	// Use this for initialization
 	void Start () {
-		
+		translation = (this.transform.position - new Vector3(0,this.transform.position.y+Random.Range(-1f,1f),0))*translationSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	this.gameObject.transform.Rotate(rotationSpeed*Time.deltaTime,rotationSpeed*Time.deltaTime,rotationSpeed*Time.deltaTime);	
+	this.gameObject.transform.Rotate(rotationSpeed*Time.deltaTime,rotationSpeed*Time.deltaTime,rotationSpeed*Time.deltaTime);
+	this.gameObject.transform.Translate(translation*Time.deltaTime,Space.World);
+	if(this.gameObject.transform.position.y<-95||this.gameObject.transform.position.y>-5||this.gameObject.transform.position.x>100||this.gameObject.transform.position.x<-100||this.gameObject.transform.position.z<-100||this.gameObject.transform.position.z>100){
+		PhotonNetwork.Destroy(this.gameObject);
+		PhotonNetwork.Instantiate("food",new Vector3(Random.Range(-20,20),Random.Range(-95,-5),Random.Range(-20,20)), Quaternion.Euler(Random.Range(0,180),Random.Range(0,180),Random.Range(0,180)),0,null);
+
+		}
 	}
 	private void OnTriggerEnter(Collider other){
 		Debug.Log ("食物：碰撞，将要删除");
