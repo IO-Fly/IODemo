b136255a19fda327eac8df5ed47a096535e5b8c5
Onion <244081385@qq.com>
2018-06-14 16:01:04 +0800

    增加毒物预设，调整生成食物的逻辑

---
+++ Assets/Script/Controller/PoisonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "player"){
+			PhotonNetwork.Destroy(this.gameObject);
+			PhotonNetwork.InstantiateSceneObject("poisonFood", new Vector3(Random.Range(-95,95), Random.Range(-95,-5), Random.Range(-95,95)),Quaternion.Euler(Random.Range(0,180),Random.Range(0,180),Random.Range(0,180)),0,null);
+		}
+	}
+}
