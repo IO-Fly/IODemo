27daed1b86dc235be8c11a8a0647900973f1ef44
zdsky <1583244339@qq.com>
2018-06-25 18:04:35 +0800

    修改minimap
    1. minimapCamera位置：player在水底时固定在海平面、player在海面上时固定在y=100.0f平面
    2. sphereForMInimap不再随player进行缩放

--- Assets/Script/UI/PlayerMinimapUI.cs
+++ Assets/Script/UI/PlayerMinimapUI.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PlayerMinimapUI : MonoBehaviour {
 
+    public  GameObject sphereForMinimapPrefab;
     private GameObject sphereForMinimap;
 
-    void Start ()
+    private void Start ()
     {
-        sphereForMinimap = this.transform.Find("SphereForMinimap").gameObject;
+        // 初始化 sphereForMinimap
+        sphereForMinimap = GameObject.Instantiate(sphereForMinimapPrefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity) as GameObject;
+        //sphereForMinimap.transform.SetParent(this.transform, false);
 
+        // 初始化 sphereForMinimap的材质，即颜色
         Material materialForMinimap;
         if( this.GetComponent<Player>().photonView.isMine )
         {
@@ -19,8 +23,15 @@ public class PlayerMinimapUI : MonoBehaviour {
         {
             materialForMinimap = Resources.Load("Minimap_materials/red") as Material;
         }
-
         sphereForMinimap.GetComponent<Renderer>().material = materialForMinimap;
+
 	}
-	
+
+    private void Update()
+    {
+        // 更新 sphereForMinimap的位置
+        sphereForMinimap.transform.position = this.transform.position + new Vector3(0.0f, 2.0f, 0.0f);
+        
+    }
+
 }
