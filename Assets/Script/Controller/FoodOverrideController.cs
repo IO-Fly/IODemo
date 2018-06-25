18df7c161e1620fd83fcc914ed78dac97c9bd04b
Onion <244081385@qq.com>
2018-06-25 13:51:59 +0800

    调整食物刷新位置逻辑

--- Assets/Script/Controller/FoodOverrideController.cs
+++ Assets/Script/Controller/FoodOverrideController.cs
@@ -20,15 +20,15 @@ public class FoodOverrideController : MonoBehaviour {
 	this.gameObject.transform.Rotate(rotationSpeed*Time.deltaTime,rotationSpeed*Time.deltaTime,rotationSpeed*Time.deltaTime);
 	this.gameObject.transform.Translate(translation*Time.deltaTime,Space.World);
 	if(this.gameObject.transform.position.y<-95||this.gameObject.transform.position.y>-5||this.gameObject.transform.position.x>100||this.gameObject.transform.position.x<-100||this.gameObject.transform.position.z<-100||this.gameObject.transform.position.z>100){
-		//if(PhotonNetwork.isMasterClient){
-			this.gameObject.transform.position = GetRandomVector3();
-			this.translation = (this.transform.position - new Vector3(0,this.transform.position.y+Random.Range(-10f,10f),0)).normalized * translationSpeed;
-			Vector3[] Data = {this.gameObject.transform.position,this.translation,new Vector3(this.ID,0,0)};
+		if(PhotonNetwork.isMasterClient){
+			Vector3 tempPosition = GetRandomVector3();
+			Vector3 temptranslation= (this.transform.position - new Vector3(0,this.transform.position.y+Random.Range(-10f,10f),0)).normalized * translationSpeed;
+			Vector3[] Data = {tempPosition,temptranslation,new Vector3(this.ID,0,0)};
 			RaiseEventOptions options = new RaiseEventOptions();
-			options.Receivers = ReceiverGroup.Others;
+			options.Receivers = ReceiverGroup.All;
 			options.CachingOption = EventCaching.DoNotCache;
 			PhotonNetwork.RaiseEvent(4,Data,true,options);
-		//	}
+			}
 
 		}
 	}
@@ -36,15 +36,15 @@ public class FoodOverrideController : MonoBehaviour {
 		Debug.Log ("食物：碰撞，将要删除");
 		if (other.gameObject.tag == "player") {
 			Debug.Log ("食物：删除");
-		//	if(PhotonNetwork.isMasterClient){
-			this.gameObject.transform.position = GetRandomVector3();
-			this.translation = (this.transform.position - new Vector3(0,this.transform.position.y+Random.Range(-10f,10f),0)).normalized * translationSpeed;
-			Vector3[] Data = {this.gameObject.transform.position,this.translation,new Vector3(this.ID,0,0)};
+			if(other.GetComponent<Player>().photonView.isMine){
+			Vector3 tempPosition = GetRandomVector3();
+			Vector3 temptranslation= (this.transform.position - new Vector3(0,this.transform.position.y+Random.Range(-10f,10f),0)).normalized * translationSpeed;
+			Vector3[] Data = {tempPosition,temptranslation,new Vector3(this.ID,0,0)};
 			RaiseEventOptions options = new RaiseEventOptions();
 			options.Receivers = ReceiverGroup.All;
 			options.CachingOption = EventCaching.DoNotCache;
 			PhotonNetwork.RaiseEvent(4,Data,true,options);
-			//}
+			}
 		}
 	}
 	private  Vector3 GetRandomVector3(){
--- Assets/Script/Manager/FoodManager.cs
+++ Assets/Script/Manager/FoodManager.cs
@@ -12,13 +12,13 @@ public class FoodManager : Photon.PunBehaviour {
 
     //食物AI
     public int FoodAICount;
-	private int foodFlushLock=0;
+	private int[] foodFlushLock;
     public GameObject foodAIPrefab;
     public GameObject[] foodAIInstances;
 
 
 	void Awake(){
-
+		foodFlushLock = new int[FoodCount];
 		PhotonNetwork.OnEventCall += this.OnEventRaised;
 		foodInstances = new GameObject[FoodCount];
         //食物AI列表
@@ -99,10 +99,10 @@ public class FoodManager : Photon.PunBehaviour {
 			//主客户端发起重置食物位置事件
 			case 4:
 			//if(!PhotonNetwork.isMasterClient&&sender.IsMasterClient){
-				if(foodFlushLock!=0)
+				if(foodFlushLock[(int)((Vector3[])content)[2].x]!=0)
 				return;
-				this.foodFlushLock = 1;
-				StartCoroutine(SetLock());
+				this.foodFlushLock[(int)((Vector3[])content)[2].x] = 1;
+				StartCoroutine(SetLock((int)((Vector3[])content)[2].x));
 				this.foodInstances[(int)((Vector3[])content)[2].x].transform.position = ((Vector3[])content)[0];
 				this.foodInstances[(int)((Vector3[])content)[2].x].GetComponent<FoodOverrideController>().translation= ((Vector3[])content)[1];
 			//}
@@ -221,9 +221,9 @@ public class FoodManager : Photon.PunBehaviour {
         }
     }
 
-	IEnumerator SetLock(){
+	IEnumerator SetLock(int id){
 		yield return new WaitForSeconds(1);
-		this.foodFlushLock = 0;	
+		this.foodFlushLock[id] = 0;	
 	}
 
 	
