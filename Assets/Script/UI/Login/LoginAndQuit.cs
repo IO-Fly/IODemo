3107ef012a0638054d64aad093179281a89dd228
zdsky <1583244339@qq.com>
2018-06-27 20:00:24 +0800

    增加Login场景

---
+++ Assets/Script/UI/Login/LoginAndQuit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginAndQuit : MonoBehaviour
+{
+
+    public void OnQuitGame()
+    {
+        Application.Quit();
+    }
+}
