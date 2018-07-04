2574d11bf9bc3fe78993e8fcf2401d23fb342326
Onion <244081385@qq.com>
2018-07-04 17:37:09 +0800

    菜单UI初步布局

---
+++ Assets/Script/UI/GameScene/MenuUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUI : MonoBehaviour {
+	private bool isActive = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			Debug.Log("esc按下");
+			if(isActive){
+				this.gameObject.SetActive(false);
+			}
+			else{
+				this.gameObject.SetActive(true);
+			}
+			
+		}
+	}
+}
