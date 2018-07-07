7054dde4bec18f4f3b13ec6b11b0dbd33d0492b0
Onion <244081385@qq.com>
2018-07-07 17:40:36 +0800

    实现了菜单的UI、更新了场景的fbx

--- Assets/Script/UI/GameScene/MenuUI.cs
+++ Assets/Script/UI/GameScene/MenuUI.cs
@@ -2,33 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MenuUI : MonoBehaviour {
 	private bool isActive = false;
+	public bool freeze = false;
+	public Sprite lose;
+	public Sprite win;
+	public Sprite pause;
 	// Use this for initialization
 	void Awake(){
-		
 	}
 	void Start () {
+		GameObject.Find("HUDCanvas").transform.Find("Menu").Find("Status").gameObject.GetComponent<Image>().sprite = this.pause;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Escape)){
+		if(Input.GetKeyDown(KeyCode.Escape)&&freeze!=true){
 			Debug.Log("esc按下");
 			if(isActive){
-				transform.Find("Menu").gameObject.SetActive(false);
+				GameObject.Find("HUDCanvas").transform.Find("Menu").gameObject.SetActive(false);
 				isActive = false;
 			}
 			else {
-				transform.Find("Menu").gameObject.SetActive(true);
+			GameObject.Find("HUDCanvas").transform.Find("Menu").gameObject.SetActive(true);
 				isActive = true;
 			}
 			
 		}
+		Debug.Log("freeze状态"+ freeze);
+		if(freeze){
+			GameObject.Find("HUDCanvas").transform.Find("Menu").Find("Continue").gameObject.SetActive(false);
+			GameObject.Find("HUDCanvas").transform.Find("Menu").gameObject.SetActive(true);
+			isActive = true;	
+		}
+				
 	}
 
 	public void ContinueGame(){
-		transform.Find("Menu").gameObject.SetActive(false);
+		GameObject.Find("HUDCanvas").transform.Find("Menu").gameObject.SetActive(false);
 		isActive = false;
 	}
 
