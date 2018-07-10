fc7cbaeaa86db82e2fc4c9cde437b1d2af8115ef
姚健钊 <1720231298@qq.com>
2018-07-10 11:18:40 +0800

    还原boundary

--- Assets/Script/Boundary.cs
+++ Assets/Script/Boundary.cs
@@ -2,31 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boundary : Photon.PunBehaviour{
+public class Boundary : Photon.PunBehaviour
+{
 
-    public enum Axis { X, Y, Z,None };
-    public Axis verticalAxis=Axis.None;//表示边界所在的面与哪一个坐标轴垂直
+    public enum Axis { X, Y, Z, None };
+    public Axis verticalAxis = Axis.None;//表示边界所在的面与哪一个坐标轴垂直
 
     public bool blockFood = true;//边界是否限制可移动食物（AI）的移动
     public bool blockPlayerAI = true;//边界是否限制玩家角色（玩家或AI控制）的移动
 
     public enum ForceDirection { Positive, Negative };//在触发器内的玩家强制更改的方向
-    public ForceDirection forceDirection=ForceDirection.Positive;
-
-	void Start () {
-        
-	}
+    public ForceDirection forceDirection = ForceDirection.Positive;
+    void Start()
+    {
+    }
 
     void Update()
     {
-        
+        if (transform.parent != null && transform.parent.position.z > 100)
+        {
+            ChangeTopEdgeAlpha();
+        }
+        if (transform.parent != null && transform.parent.position.z < -100)
+        {
+            ChangeDownEdgeAlpha();
+        }
+        if (transform.parent != null && transform.parent.position.x > 100)
+        {
+            ChangeRightEdgeAlpha();
+        }
+        if (transform.parent != null && transform.parent.position.x < -100)
+        {
+            ChangeLeftEdgeAlpha();
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
         string tag = other.gameObject.tag;
         if ((tag == "playerCopy" && blockPlayerAI)
-            || ((tag == "food" || tag == "AICollider"||tag=="foodAI") && blockFood))
+            || ((tag == "food" || tag == "AICollider" || tag == "foodAI") && blockFood))
         {
             ObjectBehaviour objectBehaviour;
             if (tag == "AICollider")
@@ -65,13 +80,13 @@ public class Boundary : Photon.PunBehaviour{
                 }
             }
         }
-           else if(tag == "player"&&other.gameObject.GetComponent<Player>().photonView.isMine){
-            if (transform.parent != null)
-                transform.parent.gameObject.GetComponent<MeshRenderer>().enabled = true;
-        }
+        /*  else if(tag == "player"&&other.gameObject.GetComponent<Player>().photonView.isMine){
+           if (transform.parent != null)
+               transform.parent.gameObject.GetComponent<MeshRenderer>().enabled = true;
+       }*/
     }
-    
-    private void OnTriggerExit(Collider other){
+
+    /*private void OnTriggerExit(Collider other){
         string tag = other.gameObject.tag;
         if(tag == "player"&&other.gameObject.GetComponent<Player>().photonView.isMine){
             if(transform.parent!=null)
@@ -79,4 +94,61 @@ public class Boundary : Photon.PunBehaviour{
         }
         
     }
+    */
+
+    private void ChangeTopEdgeAlpha()
+    {
+        Renderer render = transform.parent.gameObject.GetComponent<Renderer>();
+        if (networkManager.localPlayer.transform.position.z >= 0)
+        {
+            render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 1 - (200 - networkManager.localPlayer.transform.position.z) / 200);
+        }
+        else
+        {
+            render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 0);
+
+        }
+    }
+    private void ChangeDownEdgeAlpha()
+    {
+        Renderer render = transform.parent.gameObject.GetComponent<Renderer>();
+        if (networkManager.localPlayer.transform.position.z < 0)
+        {
+            render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 1 - (networkManager.localPlayer.transform.position.z + 200) / 200);
+        }
+        else
+        {
+            render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 0);
+
+        }
+    }
+    private void ChangeLeftEdgeAlpha()
+    {
+        Renderer render = transform.parent.gameObject.GetComponent<Renderer>();
+        if (networkManager.localPlayer.transform.position.x < 0)
+        {
+            render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 1 - (networkManager.localPlayer.transform.position.x + 200) / 200);
+        }
+        else
+        {
+            render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 0);
+
+        }
+    }
+    private void ChangeRightEdgeAlpha()
+    {
+        Renderer render = transform.parent.gameObject.GetComponent<Renderer>();
+        if (networkManager.localPlayer.transform.position.x >= 0)
+        {
+            render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 1 - (200 - networkManager.localPlayer.transform.position.x) / 200);
+        }
+        else
+        {
+            render.material.color = new Color(render.material.color.r, render.material.color.g, render.material.color.b, 0);
+
+        }
+    }
+
+
+
 }
